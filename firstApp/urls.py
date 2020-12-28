0de179c49464e8e64e6fed49cdaae57c94519540
mbharti321 <mbharti321@gmail.com>
2020-12-28 18:17:16 +0530

    add few initial urls in firstapp

--- firstApp/urls.py
+++ firstApp/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns=[
     path('',views.myFunction, name = "index"),
+    path('aboutUs', views.aboutUs, name = "aboutUs"),
+    path('add/<int:a>/<int:b>', views.functionAdd, name = "addMe"),
+    path('intro/<str:name>/<int:age>', views.functionIntro, name = "intro"),
+    path('intro2/<str:name>/<int:age>', views.functionIntro2, name = "intro2")
 ]
--- firstApp/views.py
+++ firstApp/views.py
@@ -4,3 +4,19 @@ from django.http import HttpResponse, JsonResponse
 
 def myFunction(request):
     return HttpResponse("Hola World! How are you?")
+
+def aboutUs(request):
+    return HttpResponse("About Us... Hello! This is Manish Bharti.")
+
+def functionAdd(request, a, b):
+    return HttpResponse(a+b)
+
+def functionIntro(request, name, age):
+    return HttpResponse("Hello "+ name +"! Your age is " + str(age) + ".")
+
+def functionIntro2(request, name, age):
+    introDict = {
+        "name": name,
+        "age": age
+    }
+    return JsonResponse(introDict)
