1512b6608899141a3033e854dadf12581301d432
mbharti321 <mbharti321@gmail.com>
2020-12-31 13:50:19 +0530

    style myIntroForm in myForm2.html & forms.py

--- firstApp/forms.py
+++ firstApp/forms.py
@@ -3,5 +3,8 @@ from django import forms
 
 
 class MyIntroForm(forms.Form):
-    firstName = forms.CharField(label = 'First Name', max_length = 100)
-    lastName = forms.CharField(label= "Last Name", max_length = 100)
+    # firstName = forms.CharField(label = 'First Name', max_length = 100)
+    # lastName = forms.CharField(label= "Last Name", max_length = 100)
+
+    firstName = forms.CharField(label = 'First Name', max_length = 100, widget = forms.TextInput(attrs = {"class":"form-control"}))
+    lastName = forms.CharField(label= "Last Name", max_length = 100, widget = forms.TextInput(attrs ={"class":"form-control"}))
